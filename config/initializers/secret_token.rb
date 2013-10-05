090f241eb2e17990891026fccc2c65dc67e3f6f2
nerdgrass <arboerstler@gmail.com>
2013-10-05 10:25:32 -0400

    initial commit, rails and HAML w/ basic welcome controller and route.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrellisWebsite::Application.config.secret_key_base = 'f919dc196f22c2873b38d1dcad30d206015d056b468e58adf29b73a2c87ba5476a44fdeae58c72cc0a1f2ce1e6b2482089a115b132096d9a83dc810b5c269cd2'
